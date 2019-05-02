4d706656398202a76d319d123731c332964dbcea
Rémi Monthiller <remi.monthiller@gmail.com>
2019-05-02 14:33:19 +0200

    added batch plot possibility

--- common/post_proc_batch.py
+++ common/post_proc_batch.py
@@ -9,13 +9,16 @@ r = [0.0, 0.5]
 v = [0.5, 0.0]
 b = [0.5, 0.0]
 colors = []
+batch_colors = []
 grad = len(sys.argv) - 1
 for i in range(grad):
 	c = (i/float(grad))
 	colors.append((r[0]*c+r[1]*(1-c), v[0]*c+v[1]*(1-c), b[0]*c+b[1]*(1-c)))
+	batch_colors.append((r[0]*c+r[1]*(1-c), v[0]*c+v[1]*(1-c), b[0]*c+b[1]*(1-c)))
 markers = ["$\mathbf{C}$", "$\mathbf{B}$", "$\mathbf{A}$", "d", "*", "s", "v", "o"]
+batch_markers = ["$\mathbf{C}$", "$\mathbf{B}$", "$\mathbf{A}$", "d", "*", "s", "v", "o"]
 #markers = ["$\mathbf{H}$", "$\mathbf{G}$", "$\mathbf{F}$", "$\mathbf{E}$", "$\mathbf{D}$", "$\mathbf{C}$", "$\mathbf{B}$", "$\mathbf{A}$"]
-markevery = 5
+me = 0.005 
 mew = 0.3
 ms = 7.0
 d_ad = 0
@@ -85,6 +88,7 @@ def post_process(dr):
 	# Useful constants
 	n_time = len(time) - 1
 	profilesT = data["profiles"]
+	# Averaging
 	if pPP.mean_over_time_enable:
 		i_deb = 0
 		while time[i_deb] < pPP.mean_begin_time:
@@ -116,7 +120,6 @@ def post_process(dr):
 		for j in range(len(z_star)):
 			qs += tmp_phi[j] * tmp_vx[j] * dz 
 		qsT.append(qs)
-	
 	# Computation of qf
 	qfT = []
 	for i in range(n_time + 1):
@@ -127,62 +130,106 @@ def post_process(dr):
 		for j in range(len(z_star)):
 			qf += (1.0 - tmp_phi[j]) * tmp_vf[j] * dz
 		qfT.append(qf)
+
+	# Averaging
+	qs_mean = 0
+	qf_mean = 0
+	sh_mean = 0
+	if pPP.mean_over_time_enable:
+		i_deb = 0
+		while time[i_deb] < pPP.mean_begin_time:
+			i_deb += 1
+		i = i_deb
+		while i < n_time + 1 and time[i] < pPP.mean_end_time:
+			qs_mean += qsT[i]
+			qf_mean += qfT[i]
+			sh_mean += data["shields"][i]
+			i += 1
+		qs_mean /= (i - i_deb)
+		qf_mean /= (i - i_deb)
+		sh_mean /= (i - i_deb)
+	else :
+		qs_mean = qsT[n_time]
+		qf_mean = qfT[n_time]
+		sh_mean = data["shields"][n_time]
+
+	### Storing 2D data
+	batch_val = eval(pPP.batch_plot_param)
+	if batch_val in batch_data:
+		batch_data[batch_val]["qs"].append(qs_mean)
+		batch_data[batch_val]["qf"].append(qf_mean)
+		batch_data[batch_val]["sh"].append(sh_mean)
+	else:
+		batch_data[batch_val] = {
+				"qs":[qs_mean],
+				"qf":[qf_mean],
+				"sh":[sh_mean]
+				}
 	
-	### Ploting figures
-	print(sep + "Ploting data.")
-	# Phi plot
-	axs["phi"].plot(phi, z_star, color=c, marker=m, markevery=markevery, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
-	# Vx plot
-	axs["vx"].plot(vx, z_star, color=c, marker=m, markevery=markevery, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
-	# Vxf plot
-	axs["vxf"].plot(vxf, z_star, color=c, marker=m, markevery=markevery, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
-	# Qs plot
-	axs["qs"].plot(time, qsT, color=c, marker=m, markevery=markevery, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
-	# Qf plot
-	axs["qf"].plot(time, qfT, color=c, marker=m, markevery=markevery, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
-	# Shields plot
-	axs["sh"].plot(time, data["shields"], color=c, marker=m, markevery=markevery, markeredgewidth=mew, markerfacecolor=c, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
+	if not pPP.batch_plot_enable:
+		### Ploting figures
+		print(sep + "Ploting data.")
+		# Setting markeverys
+		markevery = int(max(1.0, me * len(z_star))) 
+		markeveryT = int(max(1.0, me * len(time)))
+		# Phi plot
+		axs["phi"].plot(phi, z_star, color=c, marker=m, markevery=markevery, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
+		# Vx plot
+		axs["vx"].plot(vx, z_star, color=c, marker=m, markevery=markevery, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
+		# Vxf plot
+		axs["vxf"].plot(vxf, z_star, color=c, marker=m, markevery=markevery, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
+		# Qs plot
+		axs["qs"].plot(time, qsT, color=c, marker=m, markevery=markeveryT, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
+		# Qf plot
+		axs["qf"].plot(time, qfT, color=c, marker=m, markevery=markeveryT, markerfacecolor=c, markeredgewidth=mew, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
+		# Shields plot
+		axs["sh"].plot(time, data["shields"], color=c, marker=m, markevery=markeveryT, markeredgewidth=mew, markerfacecolor=c, markersize=ms, label=r"$"+name_param+"="+name_value+"$")
 
-### Creating figures
-figs = {}
-axs = {}
-# Phi profile
-figs["phi"] = plt.figure()
-axs["phi"] = plt.gca()
-plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of the final $\phi$ profile for different $" + name_param + "$.")
-plt.xlabel(r"$\phi$")
-plt.ylabel(r"$z/"+pPP.d_ad_type+"$")
-# Vx profile
-figs["vx"] = plt.figure()
-axs["vx"] = plt.gca()
-plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of the final $V^p_x$ profile for different $" + name_param + "$.")
-plt.xlabel(r"$V^p_x$")
-plt.ylabel(r"$z/"+pPP.d_ad_type+"$")
-# Vxf profile
-figs["vxf"] = plt.figure()
-axs["vxf"] = plt.gca()
-plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of the final $V^f_x$ profile for different $" + name_param + "$.")
-plt.xlabel(r"$V^f_x$")
-plt.ylabel(r"$z/"+pPP.d_ad_type+"$")
-# Qs over time
-figs["qs"] = plt.figure()
-axs["qs"] = plt.gca()
-plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of $Q_s$ over time for different $" + name_param + "$.")
-plt.xlabel(r"t (s)")
-plt.ylabel("$Q_s$")
-# Qf over time
-figs["qf"] = plt.figure()
-axs["qf"] = plt.gca()
-plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of $Q_f$ over time for different $" + name_param + "$.")
-plt.xlabel(r"t (s)")
-plt.ylabel("$Q_f$")
-# Shields over time
-figs["sh"] = plt.figure()
-axs["sh"] = plt.gca()
-plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of $\theta$ over time for different $" + name_param + "$.")
-plt.xlabel(r"t (s)")
-plt.ylabel(r"$\theta$")
+if not pPP.batch_plot_enable:
+	### Creating figures
+	figs = {}
+	axs = {}
+	# Phi profile
+	figs["phi"] = plt.figure()
+	axs["phi"] = plt.gca()
+	plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of the final $\phi$ profile for different $" + name_param + "$.")
+	plt.xlabel(r"$\phi$")
+	plt.ylabel(r"$z/"+pPP.d_ad_type+"$")
+	# Vx profile
+	figs["vx"] = plt.figure()
+	axs["vx"] = plt.gca()
+	plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of the final $V^p_x$ profile for different $" + name_param + "$.")
+	plt.xlabel(r"$V^p_x$")
+	plt.ylabel(r"$z/"+pPP.d_ad_type+"$")
+	# Vxf profile
+	figs["vxf"] = plt.figure()
+	axs["vxf"] = plt.gca()
+	plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of the final $V^f_x$ profile for different $" + name_param + "$.")
+	plt.xlabel(r"$V^f_x$")
+	plt.ylabel(r"$z/"+pPP.d_ad_type+"$")
+	# Qs over time
+	figs["qs"] = plt.figure()
+	axs["qs"] = plt.gca()
+	plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of $Q_s$ over time for different $" + name_param + "$.")
+	plt.xlabel(r"t (s)")
+	plt.ylabel("$Q_s$")
+	# Qf over time
+	figs["qf"] = plt.figure()
+	axs["qf"] = plt.gca()
+	plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of $Q_f$ over time for different $" + name_param + "$.")
+	plt.xlabel(r"t (s)")
+	plt.ylabel("$Q_f$")
+	# Shields over time
+	figs["sh"] = plt.figure()
+	axs["sh"] = plt.gca()
+	plt.title(r"\textbf{" + name_case.capitalize() +  r" : } Evolution of $\theta$ over time for different $" + name_param + "$.")
+	plt.xlabel(r"t (s)")
+	plt.ylabel(r"$\theta$")
 
+# Declaring batch data storage
+batch_data = {}
+
+# Processing 1D data
 for dr in sys.argv[1:]:
 	print(bigSep + dr)
 	execfile(dr+"/params.py")
@@ -196,22 +243,40 @@ for dr in sys.argv[1:]:
 		d_ad = pP.d
 	post_process(dr)
 
-#### Creating rectangular patch to show averaging
-if pPP.mean_over_time_enable:
-	rect = plt.Rectangle((pPP.mean_begin_time, 0.0), pPP.mean_end_time - pPP.mean_begin_time, 1000, facecolor='w', edgecolor='k', hatch='/', alpha=0.3)
-	axs["qs"].add_patch(rect)
-	rect2 = plt.Rectangle((pPP.mean_begin_time, 0.0), pPP.mean_end_time - pPP.mean_begin_time, 1000, facecolor='w', edgecolor='k', hatch='/', alpha=0.3)
-	axs["sh"].add_patch(rect2)
-	rect3 = plt.Rectangle((pPP.mean_begin_time, 0.0), pPP.mean_end_time - pPP.mean_begin_time, 1000, facecolor='w', edgecolor='k', hatch='/', alpha=0.3)
-	axs["qf"].add_patch(rect3)
-
-## Adding legends
-for key in axs:
-	axs[key].legend()
-
-### Saving figures
-for key in figs:
-	figs[key].savefig(save_fig_dir+name_case+"_"+name_param+"_"+key+".pdf")
+if pPP.batch_plot_enable:
+	# Plotting batch
+	plt.figure()
+	plt.title(r"\begin{center}"+
+			r"\textbf{" + name_case.capitalize() +  r" : } Evolution of the solid transport compared to the shields number \\ for different $" + pPP.batch_param_name + r"$."
+			+r"\end{center}"
+			)
+	plt.xlabel(r"$\theta$")
+	plt.ylabel(r"$Q_s$")
+	# Plotting
+	for key in batch_data:
+		batch_m = batch_markers.pop()
+		batch_c = batch_colors.pop()
+		d = batch_data[key]
+		markeveryS = int(max(1.0, me * len(d["sh"])))
+		plt.plot(d["sh"], d["qs"], color=batch_c, marker=batch_m, markevery=markeveryS, markeredgewidth=mew, markerfacecolor=batch_c, markersize=ms, label=r"$"+pPP.batch_param_name+"="+str(key)+"$")
+	plt.legend()
+else:
+	#### Creating rectangular patch to show averaging
+	if pPP.mean_over_time_enable:
+		rect = plt.Rectangle((pPP.mean_begin_time, 0.0), pPP.mean_end_time - pPP.mean_begin_time, 1000, facecolor='w', edgecolor='k', hatch='/', alpha=0.3)
+		axs["qs"].add_patch(rect)
+		rect2 = plt.Rectangle((pPP.mean_begin_time, 0.0), pPP.mean_end_time - pPP.mean_begin_time, 1000, facecolor='w', edgecolor='k', hatch='/', alpha=0.3)
+		axs["sh"].add_patch(rect2)
+		rect3 = plt.Rectangle((pPP.mean_begin_time, 0.0), pPP.mean_end_time - pPP.mean_begin_time, 1000, facecolor='w', edgecolor='k', hatch='/', alpha=0.3)
+		axs["qf"].add_patch(rect3)
+	
+	## Adding legends
+	for key in axs:
+		axs[key].legend()
+	
+	### Saving figures
+	for key in figs:
+		figs[key].savefig(save_fig_dir+name_case+"_"+name_param+"_"+key+".pdf")
 
 ### Showing figures
 plt.show()
