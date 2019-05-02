d741d5863166e4c06621419a6b36223c45701022
Rémi Monthiller <remi.monthiller@gmail.com>
2019-05-02 14:32:28 +0200

    added batch plot parameters

--- params_post_proc.py
+++ params_post_proc.py
@@ -5,3 +5,7 @@ class pPP:
 	mean_over_time_enable = True
 	mean_begin_time = 40.0
 	mean_end_time = 100.0
+	# 2D plot parameters
+	batch_plot_enable = False
+	batch_plot_param = "pS.A"
+	batch_param_name = "A"
