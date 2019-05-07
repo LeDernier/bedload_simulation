002c5e7145bbe3e01310506b8a804c571a319756
Rémi Monthiller <remi.monthiller@gmail.com>
2019-05-07 08:50:44 +0200

    added parameters to manage solve_begin_time and save

--- params_post_proc.py
+++ params_post_proc.py
@@ -1,11 +1,11 @@
 class pPP:
 	# Adimensionalisation
-	d_ad_type = "d_max"
+	d_ad_type = "d"
 	# Mean operation 
 	mean_over_time_enable = True
 	mean_begin_time = 40.0
 	mean_end_time = 100.0
 	# 2D plot parameters
 	batch_plot_enable = False
-	batch_plot_param = "pS.A"
+	batch_plot_param = "pP.d"
 	batch_param_name = "A"
