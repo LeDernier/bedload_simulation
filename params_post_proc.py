370954a2fa845b6905de20294e8f2f66312ceb6b
Rémi Monthiller <remi.monthiller@gmail.com>
2019-05-13 16:02:05 +0200

    changed params for calmip post processing

--- params_post_proc.py
+++ params_post_proc.py
@@ -2,13 +2,13 @@ class pPP:
 	# Saving figures
 	save_figs = True
 	# Adimensionalisation
-	d_ad = "pS.d_tot"
-	d_ad_name = "l"
+	d_ad = "pS.d_max"
+	d_ad_name = "s"
 	# Mean operation 
-	mean_over_time_enable = False
-	mean_begin_time = 60.0
-	mean_end_time = 200.0
+	mean_over_time_enable = True
+	mean_begin_time = 200.0
+	mean_end_time = 400.0
 	# 2D plot parameters
-	batch_plot_enable = False 
+	batch_plot_enable = True
 	batch_plot_param = "pS.A"
 	batch_param_name = "A"
