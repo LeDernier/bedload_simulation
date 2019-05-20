d220e227b6917390eb1b872777bd4db28536a3ef
Rémi Monthiller <remi.monthiller@gmail.com>
2019-05-20 08:53:23 +0200

    minor changes in post proc params in order to post process different quantities

--- params_post_proc.py
+++ params_post_proc.py
@@ -3,10 +3,10 @@ class pPP:
 	save_fig_dir = "/home/rmonthil/Documents/post_proc/"
 	save_figs = True
 	# Adimensionalisation
-	d_ad = "pow(pS.d_vol, 3)/pow(pS.d_surf, 2)"
+	d_ad = "pow(pS.d_vol, 3.0)/pow(pS.d_surf, 2.0)"
 	d_ad_name = "d_{vs}"
 	# Mean operation 
-	mean_over_time_enable = True
+	mean_over_time_enable = False
 	mean_begin_time = 200.0
 	mean_end_time = 400.0
 	# 2D plot parameters
