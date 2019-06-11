f0e530947dc2fc5c12e355c4eccf85a3660a0462
Rémi Monthiller <remi.monthiller@gmail.com>
2019-06-11 16:46:49 +0200

    updated shake case

--- shake/framework.py
+++ shake/framework.py
@@ -4,45 +4,21 @@ execfile("../common/frameworkCreationUtils.py")
 ids = {}
 
 def frameworkCreation():
-	"""Creates the framework and returns [idHouse, [idWallFront, idWallLeft, idWallRight]], ids of some of the objects created in O.bodies.
-	
-	Parameters:
-	- [diameterPart, densPart, phiPartMax, restitCoef, partFrictAngle] -- parameters relative to the particles
+	"""Creates the framework
 	
 	"""
-	## Estimated max particle pressure from the static load
-	p_max = -(pP.rho - pF.rho) * pP.phi_max * pP.n_l * pP.d * pM.g[2]
-	
-	## Evaluate the minimal normal stiffness to be in the rigid particle limit (cf Roux and Combe 2002)
-	N_s = p_max * pP.d * 1.0e4
-	## Young modulus of the particles from the stiffness wanted.
-	E = N_s / pP.d
-	## Poisson's ratio of the particles. Classical values, does not have much influence.
-	nu = 0.5
-	## Finaly difining material
-	O.materials.append(ViscElMat(en=pP.c_r, et=0., young=E, poisson=nu, density=pP.rho, frictionAngle=pP.mu, label='mat')) 
 
 	######################################################################################
 	### Framework creation
 	######################################################################################
 
+	frCrea.defineMaterials()
 	frCrea.createPeriodicCell()
-	#frCrea.createGround()
+	frCrea.createWalls()
+#	frCrea.createGround()
 	frCrea.createRugosity()
-	#frCrea.createParticles()
-	frCrea.createClumpCloud()
-	#frCrea.addClump(center = Vector3(pM.l/2.0, pM.w/2.0, pM.z_ground + 4.0 * pP.d), d = pP.d, iter_vect = Vector3(0*pP.d, 1*pP.d, 0), n = 3)
-	#frCrea.addRandomClump(center = Vector3(pM.l/2.0, pM.w/2.0, pM.z_ground + 4.0 * pP.d), d_min = 0.5 * pP.d, d_max = 1.5 * pP.d, n = 5)
-	#frCrea.createDisplayG()
-	#frCrea.createDisplayFluidHeight()
-	#(idTetra, [idS_1, idS_2, idS_3, idS_4]) = frCrea.addTetra(center = Vector3(pM.l/2.0, pM.w/2.0, pM.z_ground + pP.d), d=pS.d_tetra, l=pS.d_tetra/2.0)
+	frCrea.createParticles()
 	
-	#ids["tetra"] = idTetra
-	#ids["s_1"] = idS_1
-	#ids["s_2"] = idS_2
-	#ids["s_3"] = idS_3
-	#ids["s_4"] = idS_4
-
 	f = open('.ids','w')
 	f.write(str(ids))
 	f.close()
--- shake/params.py
+++ shake/params.py
@@ -1,129 +1,156 @@
-"""Enter the parameters of the simulation in this file.
-	
-	Parameters:
-	--- Simulation parameters
-	- t_max		-- simulation time				-- s		-- 30.0
-	--- Macroscopic parameters
-	- n			-- number of particles			-- .		-- 10
-	- theta		-- slope angle					-- rad		-- 1.5
-	- l			-- length of the cell			-- m		-- 1.0
-	- w			-- width of the cell			-- m		-- 1.0
-	- h			-- heigth of the cell			-- m		-- 1.0
-	--- Particles :
-	- d			-- diameter of the particles	-- m		-- 6e-3
-	- rho_p		-- density of the particles		-- kg/m^3	-- 2.5e3
-	- c_r		-- restitution coefficient		-- .		-- 0.7
-	- phi_max	-- maximum volume fration		-- .		-- 0.61
-	- mu		-- particles' friction angla	-- rad		-- atan(0.5)
-	--- Fluid :
-	- rho_f		-- density of the fluid			-- kg/m^3	-- 1.0e3
-	- nu_f		-- kinematic viscosity			-- m^2/s	-- 1e-6
-	- h_f		-- fluid height (above bed)		-- m		-- 0.1
-	- dt_f		-- fluid time step				-- s		-- 1e-5
-	- t_f		-- time between two resolutions	-- s		-- 1e-2
-	-- Define fluid mesh:
-	- n_z		-- number of cells (1D)			-- .		-- 900
+"""This is the default parameter file. Do not change this file.
 
-	"""
+This file is used for convinience and backward compatibility.
+
+Copy this file in your working directory and rename it "params.py" if you
+want to use it.
+
+You can still place execfile("../common/default_params.py") on the top of 
+your "params.py" if you want to set all of your parameters to default ones 
+before changing them so that you do not need to define them all.
+
+"""
+
+# execfile("../common/defaultParams.py")
 
 import math
 
-class pM: # Param Macro
-	### Simulation parameters
-	t_max = 10.0
-	# Mesh :
-	n_z = 900
-	### Macroscopic parameters
-	alpha = 0.0 
-	l = 1.0e-1
-	w = 1.0e-1
-	h = 20.0e-1
-	z_ground = h/2.0
-	### Physical parameters
-	g_scale = 9.81
-	g = Vector3(g_scale * sin(alpha), 0, -g_scale * cos(alpha))
+### Numerical Parameters
+class pN:
+	### Time of the simulation 
+	t_max = 2.0
+	### Number of cells of the mesh
+	n_z = 825
 	### Shake
 	shake_enable = True
 	shake_period = 0.04
 	shake_intensity = 0.2
-	shake_time = 9.0
+	shake_time = t_max - 1.0
+	### Verbose
+	verbose = True
 
-class pSave: # Param Save
-	yadeSavePeriod = 0.5
-	vtkRecorderIterPeriod = 0
-
-### Particles
-class pP: # Param Particle
-	d = pM.l/10.0
-	rho = 2.5e3
-	c_r = 0.7
-	phi_max = 0.4 #0.64
-	mu = atan(0.5)
-	# Ground rugosity
-	d_rug = d
-	# Computed
-	r = d/2.0
-	n = 0
-	# Will be computed after
-	n_l = 0
-	# Attributes of the particles:w
-	vx = [0] * (pM.n_z-1)
-	phi = [0] * (pM.n_z-1)
-
-### Shape
-class pS: # Param Shape
-	A = 0.334
-	d_min_star = pP.d * ((1.0/A) - 1.0) / 2.0
-	ds = [d_min_star, pP.d, d_min_star] 
-	iter_vect = [Vector3(1.0, 0.0, 0.0)]
-	# Computation of parameters
-	# d_tot = sum(ds)
-	# ds = [d * pP.d / d_tot for d in ds]
-	d_tot = sum(ds)
-	d_min = min(ds)
-	d_max = max(ds)
+### Particle Parameters
+class pP: 
+	### Shape and length parameters of the particles
+	# Type : "clump" or "cylinder"
+	kind = "clump"
+	# Characteristic lengh taken for the adimensionalisation within the shields number.
+	dvs = 6.0e-3 
+	# Shape factor 
+	A = 3.0
+	# Small characteristic length 
+	S = (2.0 * pow(A - 1.0, 2) + 4.0)/(pow(A - 1.0, 3) + 4.0) * dvs
+	# Long Characteristic length
+	L = A * S
+	# Volume and frontale surface that will be computed differently depending on the kind 
 	vol = 0
 	surf = 0
-	for d in ds:
-		vol += math.pi * pow(d, 3) / 6.0
-		surf += math.pi * pow(d, 2) / 4.0
-	d_vol = pow(6.0 * vol / math.pi, 1.0/3.0)
-	d_surf = pow(4.0 * surf / math.pi, 1.0/2.0)
+	### Clump parameters
+	if kind == "clump":
+		# Diameter of the small spheres
+		dss = (L-S)/2.0
+		# List of the diameters of the spheres in the clump
+		ds = []
+		if dss > 0.0:
+			ds = [dss, S, dss]
+		else:
+			ds = [S]
+		# Computation of the volume and the surface of the particles
+		for d in ds:
+			vol += math.pi * pow(d, 3) / 6.0
+			surf += math.pi * pow(d, 2) / 4.0
+	# Verification
+	dvs_calc = 3.0/2.0 * vol/surf
+	### Cylinder parameters
+	# TODO
+	### Density of particles
+	rho = 2.5e3
+	### Coefficient of restitution
+	c_r = 0.5
+	### Maximum volume fraction (value set after some simulations) 
+	phi_max = 0.61
+	### Friction angle
+	mu = math.atan(0.4)
+	### Initial particle velocity and volume fraction that are given to the HydroEngine
+	v = [Vector3(0,0,0)] * (pN.n_z - 1)
+	phi = [0] * (pN.n_z - 1)
 
+### Macroscopic Parameters
+class pM: 
+	### Framework parameters
+	alpha = 0.1 
+	l = 20. * pP.dvs
+	w = 20. * pP.dvs
+	h = 200. * pP.dvs
+	z_ground = h/2.0
+	### Sediment height
+	hs = 10.0 * pP.dvs
+	### Number of Particles
+	n = pP.phi_max * l * w * hs / pP.vol
+	# Number of particles "layers"
+	n_l = n / (pP.phi_max * w * l * pP.S / pP.vol)
+	### Gravity parameters
+	g_scale = 9.81
+	g = Vector3(g_scale * math.sin(alpha), 0, -g_scale * math.cos(alpha))
+	### Ground Rugosity
+	d_rug = pP.S
 
-# Computing n_l and n_ll
-pP.n = 12.0 * (pP.phi_max * pM.l * pM.w * pS.d_max / pS.vol)
-pP.n_l = pP.n / (pP.phi_max * pM.l * pM.w * pS.d_max / pS.vol)
-print("Number of particles : " + str(pP.n))
-print("Estimated number of particle layers : " + str(pP.n_l))
+### Param Save
+class pSave:
+	# Data will be saved all "yadeSavePeriod" simulation (virtual) time. Disable saving by setting it to 0.
+	yadeSavePeriod = 0.1
+	# Data will be saved as vtk (for Paraview for example) all "vtkRecorderIterPeriod" iterations. Disable saving by setting it to 0.
+	vtkRecorderIterPeriod = 0
 
-### Fluid
-class pF: # Param Fluid
+### Param Fluid
+class pF: 
 	enable = False
 	solve = False
+	# Can be "old" or "new"
+	method = "new"
 	solve_begin_time = 0.8
 	## Physics
-	rho = 1e3
-	nu = 1e-6
-	init_shields = 0.2
+	rho = 0e3
+	nu = 0e-6
+	init_shields = 0.55
 	shields = 0.0 # Will be updated during the simulation. max(hydroEngine.ReynoldStresses)/((densPart-densFluidPY)*diameterPart*abs(gravityVector[2]))
-	shields_d = pS.d_max
-	if pM.alpha != 0.0:
-		h = init_shields * (pP.rho/rho - 1) * shields_d / sin(pM.alpha)
-	else:
-		h = 0.0
-	print("Estimated fluid height : " + str(h))
+	shields_d = pP.dvs
+	# Fluid heigth
+	h = 0.5 * pM.hs
+	if pM.alpha != 0:
+		h = init_shields * (pP.rho/rho - 1) * shields_d / math.sin(pM.alpha)
 	dt = 1e-5
 	t = 1e-2
 	## Fluid mesh
 	# Richardson Zaki exponent for the hindrance function of the drag force applied to the particles ???
 	expoDrag = 3.1
 	# Computed parameters
-	z = pP.n_l * pS.d_max + h + pM.z_ground
-	dz = (z-pM.z_ground)/float(pM.n_z-1)
+	z = pM.hs + h + pM.z_ground
+	dz = (z-pM.z_ground)/float(pN.n_z-1)
 	# Attributes of the fluid
-	vx = [0] * (pM.n_z+1)
+	vx = [0] * (pN.n_z+1)
 	# Display parameters
 	display_enable = False
 	display_n = 100
 	display_mult = 0
+	# Mostly useless parameters
+	enable_wall_friction = False
+	enable_fluctuations = False
+	t_fluct = 1e-1
+
+if pN.verbose:
+	print("\n")
+
+	print("INFO: Particle volume : " +  str(pP.vol))
+	print("INFO: Particle surface : " +  str(pP.surf))
+	print("INFO: dvs:" + str(pP.dvs) + " dvs_calc:" + str(pP.dvs_calc))
+	print("INFO: Diameter of the spheres: " + str(pP.ds))
+
+	print("INFO: Number of particles : " + str(pM.n))
+	print("INFO: Estimated number of particle layers : " + str(pM.n_l))
+	
+	
+	if pF.enable:
+		print("INFO: Estimated fluid height : " + str(pF.h))
+
+	print("\n")
